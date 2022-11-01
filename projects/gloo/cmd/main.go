bbf7a1aceba6e5ad104e1a19495e5ed2fd4c3788
Kevin Dorosh <kevin.dorosh@solo.io>
2022-11-01 11:11:32 -0400

    Add race

--- projects/gloo/cmd/main.go
+++ projects/gloo/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/solo-io/gloo/pkg/utils/probes"
 	"github.com/solo-io/gloo/projects/gloo/pkg/setup"
@@ -10,6 +11,22 @@ import (
 )
 
 func main() {
+
+	// add a race for race detector to pick up:
+	// from https://go.dev/doc/articles/race_detector
+	c := make(chan bool)
+	m := make(map[string]string)
+	go func() {
+		m["1"] = "a" // First conflicting access.
+		c <- true
+	}()
+	m["2"] = "b" // Second conflicting access.
+	<-c
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	// end race
+
 	ctx := context.Background()
 	probes.StartLivenessProbeServer(ctx)
 	stats.ConditionallyStartStatsServer()
